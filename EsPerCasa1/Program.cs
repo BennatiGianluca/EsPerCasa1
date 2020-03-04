b7a344e3bd1784f196631a647c73ec73db265132
studenti <studenti@rosselli.loc>
2020-03-04 12:59:40 +0100

    Add project files.

---
+++ EsPerCasa1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EsPerCasa1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {int n;
+            do
+            {
+                Console.WriteLine("inserisci un numero");
+                n = int.Parse(Console.ReadLine());
+            }
+            while (n < 0);
+            if (n % 2 == 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i.ToString() + " ");
+                    }
+                }
+            }
+            else
+            {
+                for(int i = 1; i <= n; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.Write(i.ToString() + " ");
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
